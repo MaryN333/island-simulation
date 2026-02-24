3a3eff302f8fda220ec51dd8b4b4e3c8ec62fd4a
MaryN333 <sidyakina@gmail.com>
2026-02-24 18:32:02 +0100

    Add Island model class

--- src/main/java/cz/wz/marysidy/island/model/Island.java
+++ src/main/java/cz/wz/marysidy/island/model/Island.java
@@ -1,4 +1,51 @@
 package cz.wz.marysidy.island.model;
 
 public class Island {
+    private final int width;
+    private final int height;
+    private final Location[][] locations;
+
+    public Island(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.locations = new Location[height][width];
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                locations[y][x] = new Location(x, y);
+
+            }
+        }
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Location[][] getLocations() {
+        return locations;
+    }
+
+    public Location getLocation(int x, int y) {
+        if (!isValidCoordinate(x, y)) {
+            throw new IllegalArgumentException("Invalid coordinates: " + x + ", " + y);
+        }
+        return locations[y][x];
+    }
+
+    public boolean isValidCoordinate(int x, int y) {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
+
+    public void movePhase(){}
+    public void eatPhase(){}
+    public void reproducePhase(){}
+
+
+
+
 }
