78f56cd2ce72c307cb3edb7597f2a4cf6fe59aa2
MaryN333 <sidyakina@gmail.com>
2026-02-24 18:41:39 +0100

    Add Location model class

--- src/main/java/cz/wz/marysidy/island/model/Location.java
+++ src/main/java/cz/wz/marysidy/island/model/Location.java
@@ -1,4 +1,50 @@
 package cz.wz.marysidy.island.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Location {
+    private final int x;
+    private final int y;
+
+    private final List<Animal> animals;
+    private final List<Plant> plants;
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.animals = new ArrayList<>();
+        this.plants = new ArrayList<>();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public List<Plant> getPlants() {
+        return plants;
+    }
+
+    public void addAnimal(Animal animal){
+        animals.add(animal);
+    }
+    public void removeAnimal(Animal animal){
+        animals.remove(animal);
+    }
+
+    public void addPlant(Plant plant){
+        plants.add(plant);
+    }
+    public void removePlant(Plant plant){
+        plants.remove(plant);
+    }
+
 }
