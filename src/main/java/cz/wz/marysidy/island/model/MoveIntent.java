dae7e8e972efd7ad2958e66409af803ea8c093c9
MaryN333 <sidyakina@gmail.com>
2026-02-25 18:06:54 +0100

    Add MoveIntent model class

--- src/main/java/cz/wz/marysidy/island/model/MoveIntent.java
+++ src/main/java/cz/wz/marysidy/island/model/MoveIntent.java
@@ -1,2 +1,19 @@
-package cz.wz.marysidy.island.model;public class MoveIntent {
+package cz.wz.marysidy.island.model;
+
+public class MoveIntent {
+    private final int dx;
+    private final int dy;
+
+    public MoveIntent(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
 }
