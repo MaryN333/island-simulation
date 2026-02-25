5f8800e2534cb02e500283e6554cb6d9a27ee403
MaryN333 <sidyakina@gmail.com>
2026-02-25 18:05:14 +0100

    Add Plant model abstract class

--- src/main/java/cz/wz/marysidy/island/model/Plant.java
+++ src/main/java/cz/wz/marysidy/island/model/Plant.java
@@ -1,4 +1,36 @@
 package cz.wz.marysidy.island.model;
 
-public abstract class Plant {
+public abstract class Plant implements Organism {
+
+    private final double weight;
+    private boolean alive;
+    private Location location;
+
+    protected Plant(double weight) {
+        this.weight = weight;
+        this.alive = true;
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return alive;
+    }
+
+    @Override
+    public void die() {
+        this.alive = false;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    void setLocation(Location location) {
+        this.location = location;
+    }
 }
