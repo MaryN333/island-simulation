5599ecd1102555aa8a45305f6827bfda6a96eb47
MaryN333 <sidyakina@gmail.com>
2026-02-26 21:47:10 +0100

    Add Herbivore model abstract class

---
+++ src/main/java/cz/wz/marysidy/island/model/Herbivore.java
@@ -0,0 +1,8 @@
+package cz.wz.marysidy.island.model;
+
+public abstract class Herbivore extends Animal {
+
+    protected Herbivore(double weight, int maxSpeed, double foodRequired) {
+        super(weight, maxSpeed, foodRequired);
+    }
+}
