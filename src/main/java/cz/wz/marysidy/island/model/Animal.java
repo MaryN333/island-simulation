9e99fe30d7c62ec57a1e0355b17dd4b4716d2cfb
MaryN333 <sidyakina@gmail.com>
2026-02-26 22:02:10 +0100

    Update Animal model abstract class

--- src/main/java/cz/wz/marysidy/island/model/Animal.java
+++ src/main/java/cz/wz/marysidy/island/model/Animal.java
@@ -1,26 +1,46 @@
 package cz.wz.marysidy.island.model;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public abstract class Animal implements Organism{
     private final double weight;
     private final int maxSpeed;
     private final double foodRequired;
 
     private double currentFood;
-    private boolean alive;
+    private boolean alive = true;
     private Location location;
 
-    public Animal(double weight, int maxSpeed, double foodRequired) {
+    protected Animal(double weight, int maxSpeed, double foodRequired) {
         this.weight = weight;
         this.maxSpeed = maxSpeed;
         this.foodRequired = foodRequired;
-
         this.currentFood = foodRequired;
-        this.alive = true;
     }
 
     @Override
     public double getWeight() {
         return weight;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public double getFoodRequired() {
+        return foodRequired;
+    }
+
+    public double getCurrentFood() {
+        return currentFood;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    void moveTo(Location newLocation) {
+        this.location = newLocation;
     }
 
     @Override
@@ -33,11 +53,14 @@ public abstract class Animal implements Organism{
         alive = false;
     }
 
-    public Location getLocation() {
-        return location;
+    public void decreaseFood(double amount) {
+        currentFood -= amount;
+        if (currentFood <= 0) {
+            die();
+        }
     }
 
-    public void restoreFood(double amount) {
+    protected void restoreFood(double amount) {
         currentFood = Math.min(foodRequired, currentFood + amount);
     }
 
@@ -45,13 +68,28 @@ public abstract class Animal implements Organism{
         return currentFood < foodRequired;
     }
 
-    public int getMaxSpeed() {
-        return maxSpeed;
+
+    public MoveIntent decideMove() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+
+        int steps = random.nextInt(0, maxSpeed + 1);
+        int direction = random.nextInt(4);
+
+        int dx = 0;
+        int dy = 0;
+
+        switch (direction) {
+            case 0 -> dx = steps;
+            case 1 -> dx = -steps;
+            case 2 -> dy = steps;
+            case 3 -> dy = -steps;
+        }
+        return new MoveIntent(dx, dy);
     }
 
-    void moveTo(Location newLocation) {
-        this.location = newLocation;
-    }
+    public abstract void eat(Location location);
 
-    public abstract MoveIntent decideMove();
+    protected abstract Animal createChild();
+
+    public abstract int getMaxCountPerLocation();
 }
