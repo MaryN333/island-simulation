02690f7c5a39d99f6f0cc557b51d66f058f8c9d4
MaryN333 <sidyakina@gmail.com>
2026-02-26 21:45:18 +0100

    Add Predator model abstract class

---
+++ src/main/java/cz/wz/marysidy/island/model/Predator.java
@@ -0,0 +1,10 @@
+package cz.wz.marysidy.island.model;
+
+public abstract class Predator extends Animal {
+
+    protected Predator(double weight, int maxSpeed, double foodRequired) {
+        super(weight, maxSpeed, foodRequired);
+    }
+
+
+}
