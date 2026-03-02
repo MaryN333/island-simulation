c515d24545a2aa90c2759cb0bf9c13cc5540352e
MaryN333 <sidyakina@gmail.com>
2026-03-02 17:53:11 +0100

    Update The Wolf class.

--- src/main/java/cz/wz/marysidy/island/model/Wolf.java
+++ src/main/java/cz/wz/marysidy/island/model/Wolf.java
@@ -1,12 +1,20 @@
 package cz.wz.marysidy.island.model;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Wolf extends Predator {
     private static final double WEIGHT = 50;
     private static final int MAX_SPEED = 3;
     private static final double FOOD_REQUIRED = 8;
     private static final int MAX_COUNT_PER_LOCATION = 30;
+
+    private static final Map<Class<? extends Animal>, Integer> FOOD_MAP = new HashMap<>();
+
+    static {
+        FOOD_MAP.put(Rabbit.class, 60);
+//        FOOD_MAP.put(Mouse.class, 60);
+    }
 
     public Wolf() {
         super(WEIGHT, MAX_SPEED, FOOD_REQUIRED);
@@ -17,21 +25,17 @@ public class Wolf extends Predator {
     }
 
     @Override
-    public MoveIntent decideMove() {
-        ThreadLocalRandom random = ThreadLocalRandom.current();
-
-        int dx = random.nextInt(-MAX_SPEED, MAX_SPEED + 1);
-        int dy = random.nextInt(-MAX_SPEED, MAX_SPEED + 1);
-
-        return new MoveIntent(dx, dy);
-    }
-
-    @Override
-    public void eat(Location location) {
-    }
-
-    @Override
     protected Animal createChild() {
         return new Wolf();
+    }
+
+    @Override
+    protected int getEatProbability(Animal animal) {
+//        return FOOD_MAP.getOrDefault(animal.getClass(), 0);
+        //for test eatPhase() in Island
+        if (animal instanceof Rabbit) {
+            return 100;
+        }
+        return 0;
     }
 }
