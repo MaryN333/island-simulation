48452251188eadffddbb56ac815ef7bd2b969993
MaryN333 <sidyakina@gmail.com>
2026-03-02 17:38:41 +0100

    Update: The implementation of the eat() in Herbivore abstract class.

--- src/main/java/cz/wz/marysidy/island/model/Herbivore.java
+++ src/main/java/cz/wz/marysidy/island/model/Herbivore.java
@@ -1,8 +1,29 @@
 package cz.wz.marysidy.island.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Herbivore extends Animal {
 
     protected Herbivore(double weight, int maxSpeed, double foodRequired) {
         super(weight, maxSpeed, foodRequired);
+    }
+
+    @Override
+    public void eat(Location location) {
+        if (!isHungry()) {
+            return;
+        }
+
+        List<Plant> plantsCopy = new ArrayList<>(location.getPlants());
+
+        for (Plant plant : plantsCopy) {
+            if (!plant.isAlive()) continue;
+
+            plant.die();
+            location.removePlant(plant);
+            restoreFood(plant.getWeight());
+            break;
+        }
     }
 }
