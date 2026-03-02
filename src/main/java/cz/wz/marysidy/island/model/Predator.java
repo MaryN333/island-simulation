19226135e6e9abd19e26fd6e988250ed5792582e
MaryN333 <sidyakina@gmail.com>
2026-03-02 17:43:06 +0100

    Update: The implementation of the eat() in Predator abstract class.

--- src/main/java/cz/wz/marysidy/island/model/Predator.java
+++ src/main/java/cz/wz/marysidy/island/model/Predator.java
@@ -1,10 +1,41 @@
 package cz.wz.marysidy.island.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public abstract class Predator extends Animal {
 
     protected Predator(double weight, int maxSpeed, double foodRequired) {
         super(weight, maxSpeed, foodRequired);
     }
 
+    @Override
+    public void eat(Location location) {
+        if (!isHungry()) {
+            return;
+        }
 
+        List<Animal> animalsCopy = new ArrayList<>(location.getAnimals());
+
+        for (Animal candidate : animalsCopy) {
+            if (candidate == this) continue;
+            if (!candidate.isAlive()) continue;
+
+            int probability = getEatProbability(candidate);
+
+            if (probability > 0 && tryToEat(probability)) {
+                candidate.die();
+                location.removeAnimal(candidate);
+                restoreFood(candidate.getWeight());
+                break;
+            }
+        }
+    }
+
+    private boolean tryToEat(int probability) {
+        return new Random().nextInt(100) < probability;
+    }
+
+    protected abstract int getEatProbability(Animal animal);
 }
