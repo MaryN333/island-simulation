3a2ec907f37499d36885d8e76f63fd676b3059d9
MaryN333 <sidyakina@gmail.com>
2026-03-10 16:52:16 +0100

    Feat: add parallelForEachLocation() for multithreading

--- src/main/java/cz/wz/marysidy/island/service/LocationService.java
+++ src/main/java/cz/wz/marysidy/island/service/LocationService.java
@@ -3,24 +3,34 @@ package cz.wz.marysidy.island.service;
 import cz.wz.marysidy.island.model.Island;
 import cz.wz.marysidy.island.model.Location;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public class LocationService {
     private final Island island;
+    private static final int THREADS = Runtime.getRuntime().availableProcessors();
 
     public LocationService(Island island) {
         this.island = island;
     }
 
     public void forEachLocation(Consumer<Location> action) {
-        Location[][] locations = island.getLocations();
-        int height = island.getHeight();
-        int width = island.getWidth();
+        island.forEachLocation(action);
+    }
 
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                action.accept(locations[y][x]);
-            }
+    public void parallelForEachLocation(Consumer<Location> action) {
+        ExecutorService executor = Executors.newFixedThreadPool(THREADS);
+
+        island.forEachLocation(location -> executor.submit(() -> action.accept(location)));
+        executor.shutdown();
+
+        try {
+            executor.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
+
 }
