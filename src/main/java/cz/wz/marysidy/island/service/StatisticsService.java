3d2ecee1b30f3c69664ef1a72f9ae8b1cf08bdaf
MaryN333 <sidyakina@gmail.com>
2026-03-12 19:32:27 +0100

    feat: add emoji icons to statistics, use TreeMap for sorted statistics

--- src/main/java/cz/wz/marysidy/island/service/StatisticsService.java
+++ src/main/java/cz/wz/marysidy/island/service/StatisticsService.java
@@ -3,18 +3,16 @@ package cz.wz.marysidy.island.service;
 import cz.wz.marysidy.island.model.Animal;
 import cz.wz.marysidy.island.model.Island;
 import cz.wz.marysidy.island.model.Plant;
+import cz.wz.marysidy.island.util.EmojiRegistry;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class StatisticsService {
-
     public Map<String, Integer> collectStatistics(Island island) {
-
-        Map<String, Integer> stats = new HashMap<>();
+        Map<String, Integer> stats = new TreeMap<>();
 
         island.forEachLocation(location -> {
-
             for (Animal animal : location.getAnimals()) {
                 if (!animal.isAlive()) continue;
 
@@ -29,15 +27,14 @@ public class StatisticsService {
                 stats.put(name, stats.getOrDefault(name, 0) + 1);
             }
         });
-
         return stats;
     }
 
     public void printStatistics(Island island, int tick) {
-
         System.out.println("----- Tick " + tick + " -----");
-
-        collectStatistics(island)
-                .forEach((k, v) -> System.out.println(k + ": " + v));
+        collectStatistics(island).forEach((name, count) -> {
+            String emoji = EmojiRegistry.get(name);
+            System.out.println(emoji + " " + name + ": " + count);
+        });
     }
 }
