9fc46a675ee072d2e99706cbcd45216830a989ee
MaryN333 <sidyakina@gmail.com>
2026-03-12 18:53:54 +0100

    fix(CleanupPhase): keep Location counters consistent during cleanup

--- src/main/java/cz/wz/marysidy/island/phase/CleanupPhase.java
+++ src/main/java/cz/wz/marysidy/island/phase/CleanupPhase.java
@@ -1,12 +1,28 @@
 package cz.wz.marysidy.island.phase;
 
+import cz.wz.marysidy.island.model.Animal;
 import cz.wz.marysidy.island.model.Island;
 import cz.wz.marysidy.island.model.Location;
+import cz.wz.marysidy.island.model.Plant;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CleanupPhase extends AbstractPhase {
     @Override
     protected void executeOnLocation(Island island, Location location) {
-        location.getAnimals().removeIf(animal -> !animal.isAlive());
-        location.getPlants().removeIf(plant -> !plant.isAlive());
+        List<Animal> animalsCopy = new ArrayList<>(location.getAnimals());
+        List<Plant> plantsCopy = new ArrayList<>(location.getPlants());
+        for (Animal animal : animalsCopy) {
+            if (!animal.isAlive()) {
+                location.removeAnimal(animal);
+            }
+        }
+
+        for (Plant plant : plantsCopy) {
+            if (!plant.isAlive()) {
+                location.removePlant(plant);
+            }
+        }
     }
 }
